645c339384d81ba852dc846e7bdff65093477988
Mahadi Hasan <md.mahadihasan300@gmail.com>
2023-08-26 19:40:15 +0600

    fetch resturant list with food item added also includes ms call

--- src/main/java/com/binaryextractcorp/foodcatalogue/controller/FoodCatalogueController.java
+++ src/main/java/com/binaryextractcorp/foodcatalogue/controller/FoodCatalogueController.java
@@ -1,14 +1,12 @@
 package com.binaryextractcorp.foodcatalogue.controller;
 
+import com.binaryextractcorp.foodcatalogue.dto.FoodCataloguePage;
 import com.binaryextractcorp.foodcatalogue.dto.FoodItemDTO;
 import com.binaryextractcorp.foodcatalogue.service.FoodCatalogueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/foodCatalogue")
@@ -21,5 +19,11 @@ public class FoodCatalogueController {
     public ResponseEntity<FoodItemDTO>  addFoodItem(@RequestBody FoodItemDTO foodItemDTO){
         FoodItemDTO foodItemSaved = foodCatalogueService.addFoodItem(foodItemDTO);
         return new ResponseEntity<>(foodItemSaved, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/fetchRestaurantAndFoodItemById/{restaurantId}")
+    public ResponseEntity<FoodCataloguePage> fetchRestaurantDetailsWithFoodMenue(@PathVariable Integer restaurantId){
+        FoodCataloguePage foodCataloguePage = foodCatalogueService.fetchFoodCatalougePageDetails(restaurantId);
+        return new ResponseEntity<>(foodCataloguePage,HttpStatus.OK);
     }
 }
--- src/main/java/com/binaryextractcorp/foodcatalogue/service/FoodCatalogueService.java
+++ src/main/java/com/binaryextractcorp/foodcatalogue/service/FoodCatalogueService.java
@@ -1,11 +1,16 @@
 package com.binaryextractcorp.foodcatalogue.service;
 
+import com.binaryextractcorp.foodcatalogue.dto.FoodCataloguePage;
 import com.binaryextractcorp.foodcatalogue.dto.FoodItemDTO;
+import com.binaryextractcorp.foodcatalogue.dto.Restaurant;
 import com.binaryextractcorp.foodcatalogue.entity.FoodItem;
 import com.binaryextractcorp.foodcatalogue.mapper.FoodItemMapper;
 import com.binaryextractcorp.foodcatalogue.repo.FoodItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class FoodCatalogueService {
@@ -13,8 +18,42 @@ public class FoodCatalogueService {
     @Autowired
     FoodItemRepo foodItemRepo;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
         FoodItem foodItemSavedInDb = foodItemRepo.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
         return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItemSavedInDb);
+    }
+
+    public FoodCataloguePage fetchFoodCatalougePageDetails(Integer restaurantId) {
+
+        // food item list
+        List<FoodItem> foodItemList =  fetchFoodItemList(restaurantId);
+
+        // restaurant details from another ms
+        Restaurant restaurant =  fetchRestaurantDetailsFromRestaurantMs(restaurantId);
+
+        // merge those two in one response and return
+        return createFoodCataloguePage(foodItemList,restaurant);
+
+    }
+
+    private FoodCataloguePage createFoodCataloguePage(List<FoodItem> foodItemList, Restaurant restaurant) {
+
+        FoodCataloguePage foodCataloguePage = new FoodCataloguePage();
+        foodCataloguePage.setFoodItemList(foodItemList);
+        foodCataloguePage.setRestaurant(restaurant);
+
+        return foodCataloguePage;
+    }
+
+    private Restaurant fetchRestaurantDetailsFromRestaurantMs(Integer restaurantId) {
+        return restTemplate.getForObject("http://RESTAURANT-SERVICE/restaurant/fetchRestaurantById/" + restaurantId,Restaurant.class);
+
+    }
+
+    private List<FoodItem> fetchFoodItemList(Integer restaurantId) {
+        return foodItemRepo.findByRestaurantId(restaurantId);
     }
 }
